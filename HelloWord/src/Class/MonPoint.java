5f8eaae318e8e86b3cf010992439a7258616eaa6
sene <dreatniggazee@yahoo.fr>
2020-11-06 10:56:08 +0100

    Jamal juste pour te donner une visibilité sur mon état d'avancement : Pour l'exo 3 bloqué sur l'utilisation du Thread.sleep Exo 5 comment utiliser cette formule sur la distance Et l'exo 6 en cours je suis à la création des class. Merci

---
+++ HelloWord/src/Class/MonPoint.java
@@ -0,0 +1,21 @@
+package Class;
+
+public class MonPoint {
+
+	public static void main(String[] args) {
+		
+		Point p1,p2;
+		
+		p1 = new Point(0,0);
+		p2 = new Point(12,20);
+		
+		System.out.println("p1.x = " +p1.getX() +" "+ "p1.y = "+p1.getY());
+		System.out.println("p2.x = " +p2.getX() +" "+ "p2.y = "+p2.getY());
+		
+		
+		
+		
+	}
+	
+	
+}
